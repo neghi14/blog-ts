d18b9cf9b62f751507150d280e3637c4ec3ef189
neghi14 <sambasseynengimote@gmail.com>
2023-06-15 00:35:27 +0100

    build: created frontend

--- client/src/app/articles/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default function Article(props: PageProps) {
-  return <div>{props.params.slug}</div>;
-}
--- client/src/app/articles/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Articles() {
-  return <div>Hello World</div>;
-}
