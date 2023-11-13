4b99a21c1146ac7c1fe8d3d99857bd3a832d9839
Nengimote Sambassey <sambasseynengimote@gmail.com>
2023-11-13 19:57:08 +0100

    update: created sql file

--- server/app/database/database.sql
+++ server/app/database/database.sql
@@ -6,5 +6,5 @@ CREATE TABLE users(
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) DEFAULT 'reader'
+    role VARCHAR(255) DEFAULT 'rea'
 );
